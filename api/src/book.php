ed4899c935b3dc030c9dd1a51fcfcc5de4594139
Arkadiusz Zezula <arkadiusz.zezula@gmail.com>
2017-05-16 10:51:18 +0200

    add class, index and mysql

---
+++ api/src/book.php
@@ -0,0 +1,151 @@
+<?php
+
+class Book implements JsonSerializable {
+
+    public $id;
+    public $name;
+    public $autor;
+    public $description;
+
+    public function __construct() {
+        $this->id = -1;
+        $this->name = "";
+        $this->autor = "";
+        $this->description = "";
+    }
+
+    public function jsonSerialize() {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'autor' => $this->autor,
+            'description' => $this->description
+        ];
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getAutor() {
+        return $this->autor;
+    }
+
+    public function setAutor($autor) {
+        $this->autor = $autor;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function Create(mysqli $connection) {
+        if ($this->id == -1) {
+            $sql = "INSERT INTO Book (name, autor, description) VALUES "
+                    . "('$this->name','$this->autor','$this->description')";
+            $result = $connection->query($sql);
+            if ($result == true) {
+                $this->id = $connection->insert_id;
+                return true;
+            }
+        }
+    }
+
+    public function update(mysqli $connection) {
+        if ($this->id !== -1) {
+            $sql = "UPDATE Book SET name = '$this->name', autor = '$this->autor',"
+                    . " description = '$this->description'"
+                    . "WHERE id = $this->id";
+            $result = $connection->query($sql);
+            if ($result == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function saveToDB(mysqli $connection) {
+        if ($this->id == -1) {
+            $sql = "INSERT INTO Book (name, autor, description) VALUES "
+                    . "('$this->name','$this->autor','$this->description')";
+            $result = $connection->query($sql);
+            if ($result == true) {
+                $this->id = $connection->insert_id;
+                return true;
+            }
+        } else {
+            $sql = "UPDATE Book SET name = '$this->name', autor = '$this->autor',"
+                    . " description = '$this->description'"
+                    . "WHERE id = $this->id";
+            $result = $connection->query($sql);
+            echo $connection->error;
+            var_dump($result);
+            if ($result == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function deleteFromDB(mysqli $connection) {
+        if ($this->id != -1) {
+            $sql = "DELETE FROM Book WHERE id = $this->id";
+            $result = $connection->query($sql);
+            if ($result == true) {
+                $this->id = -1;
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static public function loadFromDB(mysqli $connection) {
+        $sql = "SELECT * FROM Book";
+        $ret = array();
+        $result = $connection->query($sql);
+        if ($result == true && $result->num_rows > 0) {
+            foreach ($result as $row) {
+                $loadedBook = new Book();
+                $loadedBook->id = $row['id'];
+                $loadedBook->name = $row['name'];
+                $loadedBook->autor = $row['autor'];
+                $loadedBook->description = $row['description'];
+                $ret[] = $loadedBook;
+            }
+        }
+
+        return $ret;
+    }
+
+    static public function loadBookById(mysqli $connection, $id) {
+        $sql = "SELECT * FROM Book WHERE id=$id";
+        $result = $connection->query($sql);
+        if ($result == true && $result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+
+            $loadedBook = new Book();
+            $loadedBook->id = $row['id'];
+            $loadedBook->name = $row['name'];
+            $loadedBook->autor = $row['autor'];
+            $loadedBook->description = $row['description'];
+
+            return $loadedBook;
+        }
+        return null;
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+require_once ('api/src/connection.php');
+require_once ('api/src/book.php');
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Books</title>
+        <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+
+            <?php
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
+            ?>
+                <div class="container">
+                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    </div>
+                    <div class="col-xs-14 col-sm-4 col-md-4 col-lg-4">
+
+                        <?php
+                        $loadBook = Book::loadBookById($conn, $id);
+                        echo " " . $loadBook->getId() . " <br>";
+                        echo " " . $loadBook->getName() . " <br>";
+                        echo " " . $loadBook->getAutor() . " <br>";
+                        echo " " . $loadBook->getDescription() . " <br>";
+
+                        echo "<a href='api/books.php?id=" . $loadBook->getId() . "&name=" . $loadBook->getName() . "&autor=" . $loadBook->getAutor() . "'> Edit</a><br>";
+                        echo "<a href='api/books.php'>Return to main page</a><br>";
+            }
+                    ?>
+                </div>
+            </div>       
+
+
+        </div>
+        
+            <div class="row">
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                </div>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    <form action="api/books.php" method="POST" role="form">
+                        <legend>Books</legend>
+                        <div class="form-group">
+                            <label for="">Add new Book</label>
+                            <input rows="4" cols="50" type="textarea" class="form-control" name="name" id="name"  placeholder="Title">
+                            <input rows="4" cols="50" type="textarea" class="form-control" name="autor" id="autor" placeholder="Autor">
+                            <input rows="4" cols="50" type="textarea" class="form-control" name="description" id="description" placeholder="Description" >
+                        </div>
+                        <button id="Book" type="submit" class="btn btn-primary">Add</button>
+                    </form>
+
+                </div>
+            </div>
+        
+        <div id="new">            
+        </div>
+            
+        
+            <div id="app"></div>
+            
+            <?php   //lista książek
+            $loadAllBooks = Book::loadFromDB($conn);
+            foreach ($loadAllBooks as $value) {
+                echo "<a href='api/books.php?id=" . $value->getId() . "'> " . $value->getId() . ". " . $value->getName() . "</a><br>";
+            }
+            
+            ?>       
+
+            <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+            <script src="api/src/app.js" type="text/javascript"></script>
+    </body>
+</html>
